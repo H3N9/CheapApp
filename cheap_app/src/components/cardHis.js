93e08052da53725bb4fbab38c90f7355f0d0284a
Ramil Arthan <42642459+H3N9@users.noreply.github.com>
2021-02-17 17:16:10 +0700

    Card Histories in progress

---
+++ cheap_app/src/components/cardHis.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const CardHis = () => {
+    return (
+        <div className="card">
+
+            <div className="boxCardImg">
+                
+            </div>
+
+            <div className="textCard">
+
+            </div>
+        </div>
+    )
+}
+
+export default CardHis

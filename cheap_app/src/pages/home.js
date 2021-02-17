a1ab5b70696defd9d3153bc20cd511a4a20fc4c5
Ramil Arthan <42642459+H3N9@users.noreply.github.com>
2021-02-17 16:24:56 +0700

    toolFetch

--- cheap_app/src/pages/home.js
+++ cheap_app/src/pages/home.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/stylesPages.css'
 import homeImage from '../images/home/home.jpg'
+import {fetchData} from '../tools/fetch'
 
 
 const Home = () => {
+    const [jsonInfo, setJsonInfo] = useState({})
+    const url = "https://api.spacexdata.com/v3/info"
+
+    useEffect(() => {
+        fetchData(url, setJsonInfo)
+    }, [])
+    console.log(jsonInfo)
     return (
            <div className="boxImage">
                <img className="bigPic" src={homeImage} />
-               <h1 id="imageText">
-                    SpaceX
+               <h1 id="name-i-image">
+                    {jsonInfo.name}
                </h1>
+               <p id="detail-i-image">
+                   {jsonInfo.summary}
+               </p>
            </div>
       )
 }

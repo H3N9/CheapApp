586e5b2d7d4fc5ccfbb2a8e9765ef83e8707fe42
t <38640424+poohcid@users.noreply.github.com>
2021-02-18 19:44:07 +0700

    rocket detail inprogress

--- cheap_app/src/pages/rocketDetail.js
+++ cheap_app/src/pages/rocketDetail.js
@@ -7,21 +7,62 @@ const RocketDetail = () =>{
     const { rocketId } = useParams();
     const { rocket } = location.state
     const image1 = rocket.flickr_images[1]
+    const image2 = "https://www.gannett-cdn.com/media/2018/02/05/USATODAY/USATODAY/636534298458151698-020518-spacex-falcon-heavy-NO-TEXT-2.png"
 
     useEffect(() => {
         console.log(rocket)
     }, [])
 
     return (
+        <>
             <div className="wrapper">
                 <div className="column">
                     <h1>{rocket.rocket_name}</h1>
+                    <br></br><br></br>
+                    <p>{rocket.description}</p>
                 </div>
                 <div className="column">
                     <img className="imageBox" src={image1} />
-                    <h1>Details</h1>
+                    Details
                 </div>
             </div>
+            <div className="wrapper">
+                <div className="column">
+                    <h1>About</h1>
+                    <br></br><br></br>
+                    <p><b>first flight :</b> {rocket.first_flight}</p>
+                    <p><b>country :</b> {rocket.country}</p>
+                    <p><b>company :</b> {rocket.company}</p>
+                </div>
+                <div className="column">
+                    <img className="imageBox" src={image2} />
+                </div>
+            </div>
+            <div className="wrapper">
+                <div className="column">
+                    <h1>size</h1>
+                    <br></br><br></br>
+                    <div className="infoList">
+                        <div>
+                            <h2>height</h2>
+                            <div>
+                                <p><b>meters: </b>{rocket.height.meters}</p>
+                                <p><b>feet: </b>{rocket.height.feet}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <h2>diameter</h2>
+                        </div>
+                        <div>
+                            <h2>mass</h2>
+                        </div> 
+                    </div>
+                </div>
+                <div className="column">
+                    <img className="imageBox" src={image2} />
+                </div>
+            </div>
+        </>
     )
 }
 
